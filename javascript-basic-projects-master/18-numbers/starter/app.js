bf171570cba98ac2b4574214c86528f80af2bf66
Keithfy Lim <keithlimek96@gmail.com>
2023-09-26 17:52:13 +0800

    halfway through es6 products

--- javascript-basic-projects-master/18-numbers/starter/app.js
+++ javascript-basic-projects-master/18-numbers/starter/app.js
@@ -1,1 +1,29 @@
-console.log('numbers project');
+console.log("numbers project");
+
+const items = [...document.querySelectorAll(".number")];
+
+const updateCount = (el) => {
+  const value = parseInt(el.dataset.value);
+  console.log(value);
+  const increment = Math.ceil(value / 1000);
+  let initialValue = 0;
+  console.log(initialValue);
+  console.log(el);
+
+  const increaseCount = setInterval(() => {
+    initialValue += increment;
+    // console.log("interval ran");
+    // console.log(initialValue);
+    if (initialValue > value) {
+      el.textContent = `${value}+`;
+      clearInterval(increaseCount);
+      return;
+    }
+    el.textContent = `${initialValue}+`;
+    // el.textcontent = initialValue;
+  }, 1);
+};
+
+items.forEach((item) => {
+  updateCount(item);
+});
